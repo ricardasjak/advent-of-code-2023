4a6c7e1911c3931fcd926538339b8dd03d198656
Ričardas Jaksebaga <rij@open.ch>
2023-12-05 14:48:27 +0100

    day 5 part 2 cleanup

--- 05/sln.ts
+++ 05/sln.ts
@@ -38,8 +38,6 @@ maps.forEach(map => {
 	map.sort((a, b) => a.source - b.source);
 });
 
-// console.log(entries);
-
 const findSeedDestination = (pos: number, { dest, source, range }: Entry): number => {
 	let result = -1;
 	if (source <= pos && pos < (source + range)) {
@@ -53,13 +51,7 @@ const seedTravel = (seed: number, sets: Array<Array<Entry>>): number => {
 	let next = seed;
 	sets.forEach((map) => {
 		const original = next;
-		// const filtered = map.filter(e => e.source <= next);
-		// console.log(map.length, filtered.length);
-
 		for (let i = 0; i < map.length; i++) {
-			// if (map[i].source + map[i].range <= next) {
-			// 	continue;
-			// }
 			const d = findSeedDestination(next, map[i]);
 			if (d > -1) {
 				next = d;
@@ -71,42 +63,35 @@ const seedTravel = (seed: number, sets: Array<Array<Entry>>): number => {
 	return next
 }
 
-
 const travel1 = seeds.map(seed => seedTravel(seed, SETS));
 const result1 = Math.min(...travel1);
 
-console.time();
-
 console.log('---------');
 console.log('pt1:', result1);
-const flag = true;
 
-if (flag) {
-	// const seeds2 = [];
-	const travel2 = [];
-	let MIN_DESTINATION = 999999999999999;
-	let counter = 0;
-	for (let i = 0; i < seeds.length; i++) {
-
-		if (i % 2 === 0) {
-			let start = seeds[i];
-			console.log('seeds2 count:', seeds[i + 1].toLocaleString(), 'from', start.toLocaleString());
-			counter += seeds[i + 1];
-			console.time('seed set travel');
-			for (let j = 0; j < seeds[i + 1]; j++) {
-				// travel2.push(seedTravel(start + j, SETS));
-				const dest = seedTravel(start + j, SETS);
-				MIN_DESTINATION = Math.min(MIN_DESTINATION, dest);
-			}
-			console.timeEnd('seed set travel');
-			console.log('from', start.toLocaleString(), 'min:', MIN_DESTINATION.toLocaleString());
-		}
-	}
-	// console.log('seeds 2 count', seeds2.length);
-	// const travel2 = seeds2.map(seed => seedTravel(seed, entries));
-	const result2 = MIN_DESTINATION;//travel2.reduce((r, n) => Math.min(r, n), 999999999999);
-	console.log('pt2:', result2);
+console.time('part 2');
+let MIN_DESTINATION = 999999999999999;
+let counter = 0;
+const rangedSeeds = [];
+for (let i = 0; i < seeds.length; i = i + 2) {
+	rangedSeeds.push([seeds[i], seeds[i + 1]])
 }
-console.timeEnd();
+rangedSeeds.sort((a, b) => a[1] - b[1]);
+for (let i = 0; i < rangedSeeds.length; i++) {
+	let [start, range] = rangedSeeds[i];
+	counter += range;
+	console.time('seeds travel time in range');
+	for (let j = 0; j < range; j++) {
+		const dest = seedTravel(start + j, SETS);
+		MIN_DESTINATION = Math.min(MIN_DESTINATION, dest);
+	}
+	console.timeEnd('seeds travel time in range');
+	console.log(i + 1, 'seeds from', start.toLocaleString(), ' range:', range.toLocaleString(), 'answer:', MIN_DESTINATION.toLocaleString());
+}
+const result2 = MIN_DESTINATION;
+console.log('pt2:', result2);
+console.log('seeds total count: ', counter);
 
-// answer: 6472060
+console.timeEnd('part 2');
+
+// answer pt2: 6472060
