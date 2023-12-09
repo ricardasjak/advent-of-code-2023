df65ff15d01e16d516e61ddcd8647536dcb511aa
Ričardas Jaksebaga <rij@open.ch>
2023-12-09 12:35:27 +0100

    day 9 part 1 solved

--- 09/09.ts
+++ 09/09.ts
@@ -9,17 +9,45 @@ console.log('---------');
 let arr = input.toLowerCase().split('\n') as string[];
 arr = arr.filter(Boolean);
 
-const parse = (lines: string[]): any[] => {
-	return [];
+const parse = (lines: string[]): Array<Array<number>> => {
+	const result = lines.map(s => s.split(' ').map(Number));
+	return result;
 }
 
+const nextSequence = (sequence: number[], result: number[][]): number[][] => {
+	const r = sequence.map((n, i) => sequence[i + 1] - n);
+	r.pop();
+	result.push(r);
+	const lastSeq = result[result.length - 1];
+	if (lastSeq[lastSeq.length - 1] === 0) {
+		return result;
+	} else {
+		return nextSequence(r, result);
+	}
+}
+
+const findNext = (triangle: number[][]): number => {
+	return triangle.reduce((acc, seq) => {
+		return acc + seq[seq.length - 1];
+	}, 0);
+}
+
+
 const solvePart1 = (input: string[]): number => {
-	return 0;
+	const data = parse(input);
+
+	const sum = data.reduce((acc, sequence) => {
+		const triangle = nextSequence(sequence, [[...sequence]]);
+		const next = findNext(triangle);
+		return acc + next;
+	}, 0);
+	return sum;
 }
 
 const solvePart2 = (input: string[]): number => {
 	return 0;
 }
+
 
 console.time();
 console.log('pt1:', solvePart1(arr));
